aff48149a431f7e6a0c6de8edb8706e4ca38668f
Mathias Koch <mk@blackbird.online>
2020-05-06 08:08:54 +0200

    Fix duplicate export of pwm module

--- src/lib.rs
+++ src/lib.rs
@@ -118,14 +118,7 @@ pub mod i2c;
     feature = "stm32l4x6"
 ))]
 pub mod prelude;
-#[cfg(any(
-    feature = "stm32l4x1",
-    feature = "stm32l4x2",
-    feature = "stm32l4x3",
-    feature = "stm32l4x5",
-    feature = "stm32l4x6"
-))]
-pub mod pwm;
+
 #[cfg(any(
     feature = "stm32l4x1",
     feature = "stm32l4x2",
