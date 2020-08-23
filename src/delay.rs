9393c6e6ab31ae1d91e176f5aba48b1b71ffcd51
Vadim Kaushan <admin@disasm.info>
2020-08-23 20:34:42 +0300

    Handle limited reload register size in SYSCLK

--- src/delay.rs
+++ src/delay.rs
@@ -47,17 +47,21 @@ impl DelayMs<u8> for Delay {
 
 impl DelayUs<u32> for Delay {
     fn delay_us(&mut self, us: u32) {
-        let rvr = us * (self.clocks.sysclk().0 / 1_000_000);
+        let mut ticks = us * (self.clocks.sysclk().0 / 1_000_000);
 
-        // assert!(rvr < (1 << 24)); //TODO fix this assertion
+        while ticks > 1 {
+            let rvr = core::cmp::min(ticks - 1, 0xffffff);
 
-        self.syst.set_reload(rvr);
-        self.syst.clear_current();
-        self.syst.enable_counter();
+            self.syst.set_reload(rvr);
+            self.syst.clear_current();
+            self.syst.enable_counter();
 
-        while !self.syst.has_wrapped() {}
+            while !self.syst.has_wrapped() {}
 
-        self.syst.disable_counter();
+            self.syst.disable_counter();
+
+            ticks -= rvr + 1;
+        }
     }
 }
 
