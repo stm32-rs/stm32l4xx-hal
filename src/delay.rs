730d74f3c39555081b91311562bb3a851a82e194
Emil Fresk <emil.fresk@gmail.com>
2020-12-22 12:12:18 +0100

    Added a Delay implementation based on `cortex_m::asm::delay` (#178)
    Fmt
    
    Made DelayCM Clone/Copy
    
    Fixed error in calculation
    
    Made milliseconds more accurate
    
    Simplification

--- src/delay.rs
+++ src/delay.rs
@@ -1,11 +1,13 @@
 //! Delays
 
 use cast::u32;
+use cortex_m::asm;
 use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m::peripheral::SYST;
 
 use crate::hal::blocking::delay::{DelayMs, DelayUs};
 use crate::rcc::Clocks;
+use crate::time::Hertz;
 
 /// System timer (SysTick) as a delay provider
 pub struct Delay {
@@ -81,6 +83,60 @@ impl DelayUs<u16> for Delay {
 }
 
 impl DelayUs<u8> for Delay {
+    fn delay_us(&mut self, us: u8) {
+        self.delay_us(u32(us))
+    }
+}
+
+/// Cortex-M `asm::delay` as provider
+#[derive(Clone, Copy)]
+pub struct DelayCM {
+    sysclk: Hertz,
+}
+
+impl DelayCM {
+    /// Create a new delay
+    pub fn new(clocks: Clocks) -> Self {
+        DelayCM {
+            sysclk: clocks.sysclk(),
+        }
+    }
+}
+
+impl DelayMs<u32> for DelayCM {
+    fn delay_ms(&mut self, ms: u32) {
+        self.delay_us(ms * 1_000);
+    }
+}
+
+impl DelayMs<u16> for DelayCM {
+    fn delay_ms(&mut self, ms: u16) {
+        self.delay_ms(u32(ms));
+    }
+}
+
+impl DelayMs<u8> for DelayCM {
+    fn delay_ms(&mut self, ms: u8) {
+        self.delay_ms(u32(ms));
+    }
+}
+
+impl DelayUs<u32> for DelayCM {
+    fn delay_us(&mut self, us: u32) {
+        // Max delay is 53_687_091 us at 80 MHz
+        let ticks = self.sysclk.0 / 1_000_000;
+
+        asm::delay(ticks * us);
+    }
+}
+
+impl DelayUs<u16> for DelayCM {
+    fn delay_us(&mut self, us: u16) {
+        self.delay_us(u32(us))
+    }
+}
+
+impl DelayUs<u8> for DelayCM {
     fn delay_us(&mut self, us: u8) {
         self.delay_us(u32(us))
     }
