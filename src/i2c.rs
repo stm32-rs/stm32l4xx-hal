2cb14e1889b93d7d24d3dfd3427332e6eb7f70be
Andrei Sousa <sousandrei@gmail.com>
2021-01-04 05:21:29 +0100

    fix stm32l4x6 imports

--- src/i2c.rs
+++ src/i2c.rs
@@ -556,7 +556,7 @@ pins!(I2C1, AF4,
 ))]
 pins!(I2C2, AF4, SCL: [PB10], SDA: [PB11]);
 
-#[cfg(any(feature = "stm32l4x1", feature = "stm32l4x3"))]
+#[cfg(any(feature = "stm32l4x1", feature = "stm32l4x3", feature = "stm32l4x6"))]
 use crate::gpio::gpiob::PB8;
 
 #[cfg(any(feature = "stm32l4x1", feature = "stm32l4x6"))]
