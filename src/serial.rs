8e6a428b6443b1fc0a5679c28c60a91f9d7d79c8
unizippro <mads@astrupa.dk>
2021-02-05 12:45:15 +0100

    Interrupt fix

--- src/serial.rs
+++ src/serial.rs
@@ -679,6 +679,52 @@ macro_rules! hal {
 
                     Ok(())
                 }
+
+                /// Starts listening for an interrupt event related to reciver
+                pub fn listen(&mut self, event: Event) {
+                    let usart = unsafe{ &(*pac::$USARTX::ptr()) };
+                    let cr1 = &usart.cr1;
+                    match event {
+                        Event::Rxne => {
+                            cr1.modify(|_, w| w.rxneie().set_bit())
+                        },
+                        Event::Txe => {
+                            ()
+                        },
+                        Event::Idle => {
+                            cr1.modify(|_, w| w.idleie().set_bit())
+                        },
+                        Event::CharacterMatch => {
+                            cr1.modify(|_, w| w.cmie().set_bit())
+                        },
+                        Event::ReceiverTimeout => {
+                            cr1.modify(|_, w| w.rtoie().set_bit())
+                        },
+                    }
+                }
+
+                /// Stops listening for an interrupt event related to reciver
+                pub fn unlisten(&mut self, event: Event) {
+                    let usart = unsafe{ &(*pac::$USARTX::ptr()) };
+                    let cr1 = &usart.cr1;
+                    match event {
+                        Event::Rxne => {
+                            cr1.modify(|_, w| w.rxneie().clear_bit())
+                        },
+                        Event::Txe => {
+                            ()
+                        },
+                        Event::Idle => {
+                            cr1.modify(|_, w| w.idleie().clear_bit())
+                        },
+                        Event::CharacterMatch => {
+                            cr1.modify(|_, w| w.cmie().clear_bit())
+                        },
+                        Event::ReceiverTimeout => {
+                            cr1.modify(|_, w| w.rtoie().clear_bit())
+                        },
+                    }
+                }
             }
 
             impl Tx<pac::$USARTX> {
@@ -719,6 +765,30 @@ macro_rules! hal {
                     });
 
                     FrameSender::new(channel)
+                }
+
+                /// Starts listening for an interrupt event
+                pub fn listen(&mut self, event: Event) {
+                    let usart = unsafe{ &(*pac::$USARTX::ptr()) };
+                    let cr1 = &usart.cr1;
+                    match event {
+                        Event::Txe => {
+                            cr1.modify(|_, w| w.txeie().set_bit());
+                        },
+                        _ => { },
+                    }
+                }
+
+                /// Stops listening for an interrupt event
+                pub fn unlisten(&mut self, event: Event) {
+                    let usart = unsafe{ &(*pac::$USARTX::ptr()) };
+                    let cr1 = &usart.cr1;
+                    match event {
+                        Event::Txe => {
+                            cr1.modify(|_, w| w.txeie().clear_bit())
+                        },
+                        _ => { },
+                    }
                 }
             }
         )+
