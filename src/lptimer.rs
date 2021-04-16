16444a5ce0bda93ed679c4e65ae1b0c0bd3c272e
Johannes Draaijer <johannes@widefind.se>
2021-04-16 14:40:00 +0200

    Fix naming and some other conventions

--- src/lptimer.rs
+++ src/lptimer.rs
@@ -135,7 +135,7 @@ macro_rules! hal {
                     .arr
                     .write(|w| unsafe { w.bits(arr_value as u32) });
 
-                instance.set_cmp_match_value(compare_value);
+                instance.set_compare_match(compare_value);
 
                 instance
             }
@@ -172,28 +172,33 @@ macro_rules! hal {
                 self.enable();
             }
 
-            pub fn clear_interrupt(&mut self, event: Event) {
-                self.lptim.icr.write(|w| match event {
-                    Event::CompareMatch => w.cmpmcf().set_bit(),
-                    Event::AutoReloadMatch => w.arrmcf().set_bit(),
-                });
-            }
-
-            pub fn is_event_triggered(&self, event: Event) -> bool {
+            pub fn is_event_triggered(&self, event: Event, clear_interrupt: bool) -> bool {
                 let reg_val = self.lptim.isr.read();
-                match event {
-                    Event::CompareMatch => return reg_val.cmpm().bit_is_set(),
-                    Event::AutoReloadMatch => return reg_val.arrm().bit_is_set(),
+                let bit_is_set = match event {
+                    Event::CompareMatch => reg_val.cmpm().bit_is_set(),
+                    Event::AutoReloadMatch => reg_val.arrm().bit_is_set(),
+                };
+                if bit_is_set && clear_interrupt {
+                    self.lptim.icr.write(|w| match event {
+                        Event::CompareMatch => w.cmpmcf().set_bit(),
+                        Event::AutoReloadMatch => w.arrmcf().set_bit(),
+                    });
                 }
+                bit_is_set
             }
 
-            pub fn set_cmp_match_value(&mut self, value: u16) {
+            pub fn set_compare_match(&mut self, value: u16) {
                 // This operation is sound as compare_value is a u16, and there are 16 writeable bits
+                // Additionally, the LPTIM peripheral will always be in the enabled state when this code is called
                 self.lptim.cmp.write(|w| unsafe { w.bits(value as u32) });
             }
 
-            pub fn get_counter(&mut self) -> u32 {
-                self.lptim.cnt.read().bits()
+            pub fn get_counter(&mut self) -> u16 {
+                self.lptim.cnt.read().bits() as u16
+            }
+
+            pub fn get_arr(&mut self) -> u16 {
+                self.lptim.arr.read().bits() as u16
             }
         }
     };
@@ -201,117 +206,3 @@ macro_rules! hal {
 
 hal!(LPTIM1, lptim1, APB1R1, lptim1en, lptim1rst, lptim1sel);
 hal!(LPTIM2, lptim2, APB1R2, lptim2en, lptim2rst, lptim2sel);
-
-// impl LowPowerTimer<LPTIM1> {
-//     pub fn new(
-//         apb1r1: &mut APB1R1,
-//         ccipr: &mut CCIPR,
-//         lptim: LPTIM1,
-//         config: LowPowerTimerConfig,
-//     ) -> Self {
-//         let LowPowerTimerConfig {
-//             clock_source,
-//             count_mode,
-//             prescaler,
-//             compare_value,
-//             arr_value,
-//         } = config;
-
-//         // ARR value must be strictly greater than CMP value
-//         assert!(arr_value > compare_value);
-
-//         apb1r1.enr().modify(|_, w| w.lptim1en().set_bit());
-//         apb1r1.rstr().modify(|_, w| w.lptim1rst().set_bit());
-//         apb1r1.rstr().modify(|_, w| w.lptim1rst().clear_bit());
-
-//         // This operation is sound as `ClockSource as u8` only produces valid values
-//         ccipr
-//             .ccipr()
-//             .modify(|_, w| unsafe { w.lptim1sel().bits(clock_source as u8) });
-
-//         lptim.cfgr.modify(|_, w| unsafe {
-//             w.enc()
-//                 .bit(false)
-//                 .countmode()
-//                 .bit(count_mode as u8 > 0)
-//                 // This operation is sound as `PreScaler as u8` only produces valid values
-//                 .presc()
-//                 .bits(prescaler as u8)
-//                 .cksel()
-//                 .bit(false)
-//         });
-
-//         let mut instance = LowPowerTimer { lptim };
-
-//         instance.enable();
-
-//         // Write compare, arr, and continous mode start register _after_ enabling lptim
-//         instance.lptim.cr.modify(|_, w| w.cntstrt().bit(true));
-
-//         // This operation is sound as arr_value is a u16, and there are 16 writeable bits
-//         instance
-//             .lptim
-//             .arr
-//             .write(|w| unsafe { w.bits(arr_value as u32) });
-
-//         instance.set_cmp_match_value(compare_value);
-
-//         instance
-//     }
-
-//     fn set_enable(&mut self, enabled: bool) {
-//         self.lptim.cr.modify(|_, w| w.enable().bit(enabled));
-//     }
-
-//     fn enable(&mut self) {
-//         self.set_enable(true);
-//     }
-
-//     fn disable(&mut self) {
-//         self.set_enable(true);
-//     }
-
-//     pub fn listen(&mut self, event: Event) {
-//         // LPTIM_IER may only be modified when LPTIM is disabled
-//         self.disable();
-//         self.lptim.ier.modify(|_, w| match event {
-//             Event::CompareMatch => w.cmpmie().set_bit(),
-//             Event::AutoReloadMatch => w.arrmie().set_bit(),
-//         });
-//         self.enable();
-//     }
-
-//     pub fn unlisten(&mut self, event: Event) {
-//         // LPTIM_IER may only be modified when LPTIM is disabled
-//         self.disable();
-//         self.lptim.ier.modify(|_, w| match event {
-//             Event::CompareMatch => w.cmpmie().clear_bit(),
-//             Event::AutoReloadMatch => w.arrmie().clear_bit(),
-//         });
-//         self.enable();
-//     }
-
-//     pub fn clear_interrupt(&mut self, event: Event) {
-//         self.lptim.icr.write(|w| match event {
-//             Event::CompareMatch => w.cmpmcf().set_bit(),
-//             Event::AutoReloadMatch => w.arrmcf().set_bit(),
-//         });
-//     }
-
-//     pub fn is_event_triggered(&self, event: Event) -> bool {
-//         let reg_val = self.lptim.isr.read();
-//         match event {
-//             Event::CompareMatch => return reg_val.cmpm().bit_is_set(),
-//             Event::AutoReloadMatch => return reg_val.arrm().bit_is_set(),
-//         }
-//     }
-
-//     pub fn set_cmp_match_value(&mut self, value: u16) {
-//         // This operation is sound as compare_value is a u16, and there are 16 writeable bits
-//         self.lptim.cmp.write(|w| unsafe { w.bits(value as u32) });
-//     }
-
-//     pub fn get_counter(&mut self) -> u32 {
-//         self.lptim.cnt.read().bits()
-//     }
-// }
