f6c34e3e33a3a53e1ecb22f8cc279085b8923fce
Jennifer Glauche <mail@jglauche.de>
2021-06-21 00:09:33 +0200

    fix fmt issue

--- src/pwm.rs
+++ src/pwm.rs
@@ -6,8 +6,8 @@ use core::mem;
 use crate::hal;
 use crate::stm32::{TIM1, TIM15, TIM2};
 
-use crate::gpio::gpioa::{PA0, PA1, PA10, PA11, PA2, PA3, PA8, PA9, PA15};
-use crate::gpio::gpiob::{PB3, PB10, PB11, PB14};
+use crate::gpio::gpioa::{PA0, PA1, PA10, PA11, PA15, PA2, PA3, PA8, PA9};
+use crate::gpio::gpiob::{PB10, PB11, PB14, PB3};
 use crate::gpio::{Alternate, AlternateOD, Floating, Input, Output, PushPull, AF1, AF14};
 use crate::rcc::{Clocks, APB1R1, APB2};
 use crate::time::Hertz;
