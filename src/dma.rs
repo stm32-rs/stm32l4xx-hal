10ed9f629b29da404570052a3f17cd334b0c0883
Jonas Spanoghe <jonas@spanoghe.net>
2021-10-31 21:12:54 +0100

    src/dma.rs: disable `cselr` for stm32l4+
    The stm32l4+ devices do not have this register,
    but use a separate peripheral (DMAMUX) for routing between
    peripherals and DMA controllers.

--- src/dma.rs
+++ src/dma.rs
@@ -644,6 +644,16 @@ macro_rules! dma {
                             unsafe { &(*$DMAX::ptr()).$cmarX }
                         }
 
+                        #[cfg(not(any(
+                            // feature = "stm32l4p5",
+                            // feature = "stm32l4q5",
+                            // feature = "stm32l4r5",
+                            // feature = "stm32l4s5",
+                            // feature = "stm32l4r7",
+                            // feature = "stm32l4s7",
+                            feature = "stm32l4r9",
+                            feature = "stm32l4s9"
+                        )))]
                         #[inline]
                         pub(crate) fn cselr(&mut self) -> &dma1::CSELR {
                             unsafe { &(*$DMAX::ptr()).cselr }
