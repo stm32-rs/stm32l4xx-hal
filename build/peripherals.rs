c4299b429eaeab3ae158fdc350b53962228195b7
JC <8765278+Crzyrndm@users.noreply.github.com>
2021-12-24 18:30:01 +1300

    update peripheral feature tables based on L4 cube package v1.17.1

--- build/peripherals.rs
+++ build/peripherals.rs
@@ -14,34 +14,22 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L471
             || IS_FEATURE_ENABLED_L475
             || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
             || IS_FEATURE_ENABLED_L486
             || IS_FEATURE_ENABLED_L496
             || IS_FEATURE_ENABLED_L4A6
             || IS_FEATURE_ENABLED_L4P5
-            || IS_FEATURE_ENABLED_L4Q5
-            || IS_FEATURE_ENABLED_L4R5
-            || IS_FEATURE_ENABLED_L4S5
-            || IS_FEATURE_ENABLED_L4R7
-            || IS_FEATURE_ENABLED_L4S7
-            || IS_FEATURE_ENABLED_L4R9
-            || IS_FEATURE_ENABLED_L4S9,
+            || IS_FEATURE_ENABLED_L4Q5,
     },
     FeatureGate {
         name: "adc3",
         state: IS_FEATURE_ENABLED_L471
             || IS_FEATURE_ENABLED_L475
             || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
             || IS_FEATURE_ENABLED_L486
             || IS_FEATURE_ENABLED_L496
-            || IS_FEATURE_ENABLED_L4A6
-            || IS_FEATURE_ENABLED_L4P5
-            || IS_FEATURE_ENABLED_L4Q5
-            || IS_FEATURE_ENABLED_L4R5
-            || IS_FEATURE_ENABLED_L4S5
-            || IS_FEATURE_ENABLED_L4R7
-            || IS_FEATURE_ENABLED_L4S7
-            || IS_FEATURE_ENABLED_L4R9
-            || IS_FEATURE_ENABLED_L4S9,
+            || IS_FEATURE_ENABLED_L4A6,
     },
     FeatureGate {
         name: "dac1",
@@ -52,18 +40,50 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L443
             || IS_FEATURE_ENABLED_L451
             || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "comp1",
-        state: IS_FEATURE_ENABLED_L431
+        state: IS_FEATURE_ENABLED_L412
+            || IS_FEATURE_ENABLED_L422
+            || IS_FEATURE_ENABLED_L431
             || IS_FEATURE_ENABLED_L432
             || IS_FEATURE_ENABLED_L433
             || IS_FEATURE_ENABLED_L442
             || IS_FEATURE_ENABLED_L443
             || IS_FEATURE_ENABLED_L451
             || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "comp2",
@@ -74,26 +94,87 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L443
             || IS_FEATURE_ENABLED_L451
             || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "dfsdm1",
-        state: IS_FEATURE_ENABLED_L451 || IS_FEATURE_ENABLED_L452 || IS_FEATURE_ENABLED_L462,
+        state: IS_FEATURE_ENABLED_L451
+            || IS_FEATURE_ENABLED_L452
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "lcd",
-        state: IS_FEATURE_ENABLED_L433 || IS_FEATURE_ENABLED_L443,
+        state: IS_FEATURE_ENABLED_L433
+            || IS_FEATURE_ENABLED_L443
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6,
     },
     FeatureGate {
         name: "aes",
         state: IS_FEATURE_ENABLED_L422
             || IS_FEATURE_ENABLED_L442
             || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L462,
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "tim3",
-        state: IS_FEATURE_ENABLED_L451 || IS_FEATURE_ENABLED_L452 || IS_FEATURE_ENABLED_L462,
+        state: IS_FEATURE_ENABLED_L451
+            || IS_FEATURE_ENABLED_L452
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "tim7",
@@ -112,59 +193,97 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L443
             || IS_FEATURE_ENABLED_L451
             || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "i2c2",
+        state: !(IS_FEATURE_ENABLED_L432 || IS_FEATURE_ENABLED_L442),
     },
     FeatureGate {
         name: "i2c4",
-        state: IS_FEATURE_ENABLED_L451 || IS_FEATURE_ENABLED_L452 || IS_FEATURE_ENABLED_L462,
+        state: IS_FEATURE_ENABLED_L451
+            || IS_FEATURE_ENABLED_L452
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "usart3",
-        state: IS_FEATURE_ENABLED_L412
-            || IS_FEATURE_ENABLED_L422
-            || IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+        state: !(IS_FEATURE_ENABLED_L432 || IS_FEATURE_ENABLED_L442),
     },
     FeatureGate {
         name: "uart4",
-        state: IS_FEATURE_ENABLED_L451 || IS_FEATURE_ENABLED_L452 || IS_FEATURE_ENABLED_L462,
+        state: IS_FEATURE_ENABLED_L451
+            || IS_FEATURE_ENABLED_L452
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "uart5",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "spi2",
-        state: IS_FEATURE_ENABLED_L412
-            || IS_FEATURE_ENABLED_L422
-            || IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+        state: !(IS_FEATURE_ENABLED_L432 || IS_FEATURE_ENABLED_L442),
     },
     FeatureGate {
         name: "spi3",
-        state: IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L432
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+        state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
     },
     FeatureGate {
-        name: "sai",
-        state: IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L432
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+        name: "sai1",
+        state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
     },
     FeatureGate {
         name: "swpmi1",
@@ -172,19 +291,24 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L432
             || IS_FEATURE_ENABLED_L433
             || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443,
+            || IS_FEATURE_ENABLED_L443
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6,
     },
     FeatureGate {
         name: "sdmmc",
-        state: IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+        state: !(IS_FEATURE_ENABLED_L412
+            || IS_FEATURE_ENABLED_L422
+            || IS_FEATURE_ENABLED_L432
+            || IS_FEATURE_ENABLED_L442),
     },
     FeatureGate {
-        name: "usb_fs",
+        name: "usb_device_fs",
         state: IS_FEATURE_ENABLED_L412
             || IS_FEATURE_ENABLED_L422
             || IS_FEATURE_ENABLED_L432
@@ -195,14 +319,24 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L462,
     },
     FeatureGate {
+        name: "usb_otg_fs",
+        state: IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
         name: "can1",
-        state: IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L432
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462,
+        state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
     },
 ];
