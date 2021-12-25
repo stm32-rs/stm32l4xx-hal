40107da4b05d63055b2b9a43e51a9a9893f9b52d
JC <8765278+Crzyrndm@users.noreply.github.com>
2021-12-25 14:10:12 +1300

    add gates for pretty much every peripheral (including those that are always present)

--- build/peripherals.rs
+++ build/peripherals.rs
@@ -8,6 +8,10 @@ use crate::{features::*, FeatureGate};
 /// ```
 pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
     FeatureGate {
+        name: "adc1",
+        state: true,
+    },
+    FeatureGate {
         name: "adc2",
         state: IS_FEATURE_ENABLED_L412
             || IS_FEATURE_ENABLED_L422
@@ -59,57 +63,11 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
     },
     FeatureGate {
         name: "comp1",
-        state: IS_FEATURE_ENABLED_L412
-            || IS_FEATURE_ENABLED_L422
-            || IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L432
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462
-            || IS_FEATURE_ENABLED_L471
-            || IS_FEATURE_ENABLED_L475
-            || IS_FEATURE_ENABLED_L476
-            || IS_FEATURE_ENABLED_L485
-            || IS_FEATURE_ENABLED_L486
-            || IS_FEATURE_ENABLED_L496
-            || IS_FEATURE_ENABLED_L4A6
-            || IS_FEATURE_ENABLED_L4P5
-            || IS_FEATURE_ENABLED_L4Q5
-            || IS_FEATURE_ENABLED_L4R5
-            || IS_FEATURE_ENABLED_L4S5
-            || IS_FEATURE_ENABLED_L4R7
-            || IS_FEATURE_ENABLED_L4S7
-            || IS_FEATURE_ENABLED_L4R9
-            || IS_FEATURE_ENABLED_L4S9,
+        state: true,
     },
     FeatureGate {
         name: "comp2",
-        state: IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L432
-            || IS_FEATURE_ENABLED_L433
-            || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462
-            || IS_FEATURE_ENABLED_L471
-            || IS_FEATURE_ENABLED_L475
-            || IS_FEATURE_ENABLED_L476
-            || IS_FEATURE_ENABLED_L485
-            || IS_FEATURE_ENABLED_L486
-            || IS_FEATURE_ENABLED_L496
-            || IS_FEATURE_ENABLED_L4A6
-            || IS_FEATURE_ENABLED_L4P5
-            || IS_FEATURE_ENABLED_L4Q5
-            || IS_FEATURE_ENABLED_L4R5
-            || IS_FEATURE_ENABLED_L4S5
-            || IS_FEATURE_ENABLED_L4R7
-            || IS_FEATURE_ENABLED_L4S7
-            || IS_FEATURE_ENABLED_L4R9
-            || IS_FEATURE_ENABLED_L4S9,
+        state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
     },
     FeatureGate {
         name: "dfsdm1",
@@ -156,6 +114,14 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
+        name: "tim1",
+        state: true,
+    },
+    FeatureGate {
+        name: "tim2",
+        state: true,
+    },
+    FeatureGate {
         name: "tim3",
         state: IS_FEATURE_ENABLED_L451
             || IS_FEATURE_ENABLED_L452
@@ -177,23 +143,52 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
+        name: "tim4",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "tim5",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "tim6",
+        state: true,
+    },
+    FeatureGate {
         name: "tim7",
         state: IS_FEATURE_ENABLED_L431
             || IS_FEATURE_ENABLED_L432
             || IS_FEATURE_ENABLED_L433
             || IS_FEATURE_ENABLED_L442
-            || IS_FEATURE_ENABLED_L443,
-    },
-    FeatureGate {
-        name: "i2c2",
-        state: IS_FEATURE_ENABLED_L412
-            || IS_FEATURE_ENABLED_L422
-            || IS_FEATURE_ENABLED_L431
-            || IS_FEATURE_ENABLED_L433
             || IS_FEATURE_ENABLED_L443
-            || IS_FEATURE_ENABLED_L451
-            || IS_FEATURE_ENABLED_L452
-            || IS_FEATURE_ENABLED_L462
             || IS_FEATURE_ENABLED_L471
             || IS_FEATURE_ENABLED_L475
             || IS_FEATURE_ENABLED_L476
@@ -211,8 +206,68 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
+        name: "tim8",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "tim15",
+        state: true,
+    },
+    FeatureGate {
+        name: "tim16",
+        state: true,
+    },
+    FeatureGate {
+        name: "tim17",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "lptim1",
+        state: true,
+    },
+    FeatureGate {
+        name: "lptim2",
+        state: true,
+    },
+    FeatureGate {
+        name: "i2c1",
+        state: true,
+    },
+    FeatureGate {
         name: "i2c2",
         state: !(IS_FEATURE_ENABLED_L432 || IS_FEATURE_ENABLED_L442),
+    },
+    FeatureGate {
+        name: "i2c3",
+        state: true,
     },
     FeatureGate {
         name: "i2c4",
@@ -231,16 +286,24 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
+        name: "usart1",
+        state: true,
+    },
+    FeatureGate {
+        name: "usart2",
+        state: true,
+    },
+    FeatureGate {
         name: "usart3",
         state: !(IS_FEATURE_ENABLED_L432 || IS_FEATURE_ENABLED_L442),
     },
     FeatureGate {
         name: "uart4",
-        state: //IS_FEATURE_ENABLED_L451 -- missing PAC support
-            //|| IS_FEATURE_ENABLED_L452 -- missing PAC support
-            //|| IS_FEATURE_ENABLED_L462 -- missing PAC support
-            //|| IS_FEATURE_ENABLED_L471 -- missing PAC support
-             IS_FEATURE_ENABLED_L475
+        state: IS_FEATURE_ENABLED_L451
+            || IS_FEATURE_ENABLED_L452
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
             || IS_FEATURE_ENABLED_L476
             || IS_FEATURE_ENABLED_L485
             || IS_FEATURE_ENABLED_L486
@@ -274,6 +337,18 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
+        name: "lpuart1",
+        state: true,
+    },
+    FeatureGate {
+        name: "tim16",
+        state: true,
+    },
+    FeatureGate {
+        name: "spi1",
+        state: true,
+    },
+    FeatureGate {
         name: "spi2",
         state: !(IS_FEATURE_ENABLED_L432 || IS_FEATURE_ENABLED_L442),
     },
@@ -282,8 +357,46 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
         state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
     },
     FeatureGate {
+        name: "qspi",
+        state: IS_FEATURE_ENABLED_L412
+            || IS_FEATURE_ENABLED_L422
+            || IS_FEATURE_ENABLED_L431
+            || IS_FEATURE_ENABLED_L432
+            //|| IS_FEATURE_ENABLED_L433 -- missing PAC support
+            || IS_FEATURE_ENABLED_L442
+            //|| IS_FEATURE_ENABLED_L443 -- missing PAC support
+            || IS_FEATURE_ENABLED_L451
+            || IS_FEATURE_ENABLED_L452
+            || IS_FEATURE_ENABLED_L462
+            || IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6,
+    },
+    FeatureGate {
         name: "sai1",
         state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
+    },
+    FeatureGate {
+        name: "sai2",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "swpmi1",
@@ -301,7 +414,7 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4A6,
     },
     FeatureGate {
-        name: "sdmmc",
+        name: "sdmmc1",
         state: !(IS_FEATURE_ENABLED_L412
             || IS_FEATURE_ENABLED_L422
             || IS_FEATURE_ENABLED_L432
@@ -340,11 +453,53 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
         state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
     },
     FeatureGate {
+        name: "can2",
+        state: IS_FEATURE_ENABLED_L496 || IS_FEATURE_ENABLED_L4A6,
+    },
+    FeatureGate {
         name: "rtc_type3",
         state: IS_FEATURE_ENABLED_L412
             || IS_FEATURE_ENABLED_L422
             || IS_FEATURE_ENABLED_L4P5
             || IS_FEATURE_ENABLED_L4Q5,
+    },
+    FeatureGate {
+        name: "gpioa",
+        state: true,
+    },
+    FeatureGate {
+        name: "gpiob",
+        state: true,
+    },
+    FeatureGate {
+        name: "gpioc",
+        state: true,
+    },
+    FeatureGate {
+        name: "gpiod",
+        state: true,
+    },
+    FeatureGate {
+        name: "gpioe",
+        state: true,
+    },
+    FeatureGate {
+        name: "gpiof",
+        state: //IS_FEATURE_ENABLED_L471 -- missing PAC support
+                IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486
+            || IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
         name: "gpiog",
@@ -365,9 +520,43 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S9,
     },
     FeatureGate {
-        name: "gpiof",
-        state: //IS_FEATURE_ENABLED_L471 -- missing PAC support
-                IS_FEATURE_ENABLED_L475
+        name: "gpioh",
+        state: true,
+    },
+    FeatureGate {
+        name: "gpioi",
+        state: IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        // aka CRS
+        name: "clock_recovery_system",
+        state: !(IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
+            || IS_FEATURE_ENABLED_L476
+            || IS_FEATURE_ENABLED_L485
+            || IS_FEATURE_ENABLED_L486),
+    },
+    FeatureGate {
+        name: "dac1",
+        state: !(IS_FEATURE_ENABLED_L412 || IS_FEATURE_ENABLED_L422),
+    },
+    FeatureGate {
+        name: "opamp1",
+        state: true,
+    },
+    FeatureGate {
+        name: "opamp2",
+        state: IS_FEATURE_ENABLED_L471
+            || IS_FEATURE_ENABLED_L475
             || IS_FEATURE_ENABLED_L476
             || IS_FEATURE_ENABLED_L485
             || IS_FEATURE_ENABLED_L486
@@ -381,5 +570,67 @@ pub(crate) const PERIPHERAL_FEATURES: &[FeatureGate] = &[
             || IS_FEATURE_ENABLED_L4S7
             || IS_FEATURE_ENABLED_L4R9
             || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "dma1",
+        state: true,
+    },
+    FeatureGate {
+        name: "dma2",
+        state: true,
+    },
+    FeatureGate {
+        name: "crc",
+        state: true,
+    },
+    FeatureGate {
+        // aka touch sense controller
+        name: "tsc",
+        state: true,
+    },
+    FeatureGate {
+        // aka digital camera memory interface
+        name: "dcmi",
+        state: IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "dma2d",
+        state: IS_FEATURE_ENABLED_L496
+            || IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4R5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4R7
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4R9
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "hash",
+        state: IS_FEATURE_ENABLED_L4A6
+            || IS_FEATURE_ENABLED_L4P5
+            || IS_FEATURE_ENABLED_L4Q5
+            || IS_FEATURE_ENABLED_L4S5
+            || IS_FEATURE_ENABLED_L4S7
+            || IS_FEATURE_ENABLED_L4S9,
+    },
+    FeatureGate {
+        name: "rng",
+        state: true,
+    },
+    FeatureGate {
+        // aka flexible memory controller
+        name: "fmc",
+        state: true,
     },
 ];
