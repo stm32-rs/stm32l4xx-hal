919a532b092979b1435886540cfd3a9d1bae5882
JC <8765278+Crzyrndm@users.noreply.github.com>
2021-12-25 14:10:31 +1300

    update lib.rs gating

--- src/lib.rs
+++ src/lib.rs
@@ -10,67 +10,27 @@
 
 #![no_std]
 
-// the common lists of features to spec what a MCU is capable of
-// lists are split by 3rd digit and 2nd digit groups
-
-// L4x1
-// any(feature = "stm32l431", feature = "stm32l451", feature = "stm32l471")
-// L4x2
-// any(feature = "stm32l412", feature = "stm32l422", feature = "stm32l432", feature = "stm32l442", feature = "stm32l452", feature = "stm32l462")
-// L4x3
-// any(feature = "stm32l433", feature = "stm32l443")
-// L4x5
-// any(feature = "stm32l475")
-// L4x6
-// any(feature = "stm32l476", feature = "stm32l486", feature = "stm32l496", feature = "stm32l4a6")
-// L4+x9
-// any(feature = "stm32l4r9", feature = "stm32l4s9")
-
-// NOTE: The even member of the pair has additional hashing peripheral(s)
-// L41 / L42
-// any(feature = "stm32l412", feature = "stm32l422")
-// L43 / L44
-// any(feature = "stm32l431", feature = "stm32l432", feature = "stm32l433", feature = "stm32l442", feature = "stm32l443")
-// L45 / L46
-// any(feature = "stm32l451", feature = "stm32l452", feature = "stm32l462")
-// L47 / L48
-// any(feature = "stm32l471", feature = "stm32l475", feature = "stm32l476", feature = "stm32l486")
-// L49 / L4a
-// any(feature = "stm32l496", feature = "stm32l4a6")
-// L4r / L4s
-// any(feature = "stm32l4r9", feature = "stm32l4s9")
-
 pub use embedded_hal as hal;
 
 pub use stm32l4;
-#[cfg(any(feature = "stm32l431", feature = "stm32l451", feature = "stm32l471"))]
+#[cfg(condition = "family_L4x1")]
 pub use stm32l4::stm32l4x1 as pac;
 
-#[cfg(any(feature = "stm32l412", feature = "stm32l422"))]
+#[cfg(condition = "family_L41_42")]
 pub use stm32l4::stm32l412 as pac;
-#[cfg(any(
-    feature = "stm32l432",
-    feature = "stm32l442",
-    feature = "stm32l452",
-    feature = "stm32l462"
-))]
+#[cfg(all(condition = "family_L4x2", not(condition = "family_L41_42")))]
 pub use stm32l4::stm32l4x2 as pac;
 
-#[cfg(any(feature = "stm32l433", feature = "stm32l443"))]
+#[cfg(condition = "family_L4x3")]
 pub use stm32l4::stm32l4x3 as pac;
 
-#[cfg(any(feature = "stm32l475"))]
+#[cfg(condition = "family_L4x5")]
 pub use stm32l4::stm32l4x5 as pac;
 
-#[cfg(any(
-    feature = "stm32l476",
-    feature = "stm32l486",
-    feature = "stm32l496",
-    feature = "stm32l4a6"
-))]
+#[cfg(condition = "family_L4x6")]
 pub use stm32l4::stm32l4x6 as pac;
 
-#[cfg(any(feature = "stm32l4r9", feature = "stm32l4s9",))]
+#[cfg(condition = "family_L4+x9")]
 pub use stm32l4::stm32l4r9 as pac;
 
 #[cfg(feature = "rt")]
@@ -81,83 +41,58 @@ pub use crate::pac as stm32;
 
 pub mod traits;
 
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod adc;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
-#[cfg(not(any(feature = "stm32l412",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
+#[cfg(condition = "peripheral_can1")]
 pub mod can;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod crc;
 pub mod datetime;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod delay;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod dma;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod flash;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod gpio;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod i2c;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod lptimer;
-#[cfg(all(
-    feature = "otg_fs",
-    any(
-        feature = "stm32l475",
-        feature = "stm32l476",
-        feature = "stm32l486",
-        feature = "stm32l496",
-        feature = "stm32l4a6",
-    )
-))]
+#[cfg(all(feature = "otg_fs", condition = "peripheral_usb_otg_fs"))]
 pub mod otg_fs;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod prelude;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod pwm;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod pwr;
-#[cfg(not(any(
-    feature = "stm32l433",
-    feature = "stm32l443",
-    feature = "stm32l4r9",
-    feature = "stm32l4s9",
-)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
+#[cfg(condition = "peripheral_qspi")]
 pub mod qspi;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod rcc;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod rng;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod rtc;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod serial;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod signature;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod spi;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod time;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod timer;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod tsc;
-#[cfg(all(
-    feature = "stm32-usbd",
-    any(
-        feature = "stm32l412",
-        feature = "stm32l422",
-        feature = "stm32l432",
-        feature = "stm32l442",
-        feature = "stm32l452",
-        feature = "stm32l462",
-        feature = "stm32l433",
-        feature = "stm32l443"
-    )
-))]
+#[cfg(all(feature = "stm32-usbd", condition = "peripheral_usb_device_fs"))]
 pub mod usb;
-#[cfg(not(any(feature = "stm32l4r9", feature = "stm32l4s9",)))]
+#[cfg(not(any(condition = "family_L4+x9",)))]
 pub mod watchdog;
 
 mod sealed {
